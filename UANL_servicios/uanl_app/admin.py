2f815d757fddcb897dc0e4868d508538f488bf1e
juanGZZA <juanpablogonzalezantonio@gmail.com>
2024-08-14 22:23:08 -0600

    Se añadieron los modelos de los servicios y dependencias

--- UANL_servicios/uanl_app/admin.py
+++ UANL_servicios/uanl_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from uanl_app.models import UserProfileInfo, Noticias
+from uanl_app.models import UserProfileInfo, Noticias, Dependencia, Servicio
 # Register your models here.
 
 admin.site.register(UserProfileInfo)
 admin.site.register(Noticias)
+admin.site.register(Dependencia)
+admin.site.register(Servicio)
--- UANL_servicios/uanl_app/models.py
+++ UANL_servicios/uanl_app/models.py
@@ -23,3 +23,17 @@ class Noticias(models.Model):
     def __str__(self):
         return self.encabezado
 
+
+class Dependencia(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=264, unique=True)
+    Dependicia = models.ForeignKey(Dependencia, on_delete=models.PROTECT)
+    informacion = models.CharField(max_length=4000, unique=True)
+
+
